07445fd7c96894dd5dfae3ac50fd0d2b07ba4875
Mateus Pires <mateus.dev.ti@gmail.com>
2024-11-12 18:41:05 -0300

    ``` Remove unused SVG files and update MenuTop component
    - Delete unused SVG files from public directory
    - Implement MenuTop component with logo, search bar, and background image
    - Add custom background gradient for menu
    - Import and use new image assets
    ```
    
    This commit message summarizes the main changes:
    1. Removal of unused SVG files
    2. Implementation of the MenuTop component with new features
    3. Addition of a custom background gradient
    4. Usage of new image assets

--- src/_components/MenuTop/index.tsx
+++ src/_components/MenuTop/index.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
 import React from "react";
+import Logo from "@/public/images/logo-stjd.svg";
+import BgTop from "@/public/images/bg-fundo-menu.png";
+import { Search } from "lucide-react";
 
 function MenuTop() {
-  return <div className="container">MenuTop</div>;
+  return (
+    <div className="container mt-[1.5rem]">
+      <div className="flex justify-between items-center bg-custom-menu px-8 py-5 rounded-[1.375rem] relative">
+        <Image src={Logo} alt="Logo" />
+        <form className="max-w-[22.0625rem] z-10 w-full relative">
+          <input
+            type="text"
+            name=""
+            id=""
+            className="w-full px-[1.25rem] py-[0.88rem] rounded-[9.375rem] text-[#000000] placeholder:text-[#000000]"
+            placeholder="Procurar..."
+          />
+          <button className="absolute right-[1.5rem] top-0 bottom-0">
+            <Search />
+          </button>
+        </form>
+        <div className="absolute right-0 top-1 z-0 ">
+          <Image src={BgTop} alt="BgTop" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MenuTop;
