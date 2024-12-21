58bba9e89a756d6a1c42ce10bca2b5fc5b16f7cd
Mateus Pires <mateus.dev.ti@gmail.com>
2024-12-21 16:31:14 -0300

    ajustes menu

--- src/_components/MenuTop/index.tsx
+++ src/_components/MenuTop/index.tsx
@@ -7,14 +7,29 @@ import { Search } from "lucide-react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
-const menus = [
+interface MenuItem {
+  name: string;
+  pathname?: string;
+  paths?: { name: string; pathname: string }[]; // Submenus opcionais
+}
+
+const menus: MenuItem[] = [
   {
     name: "Início",
     pathname: "/",
   },
   {
-    name: "Quem Somos",
-    pathname: "/quem-somos",
+    name: "Institucional",
+    paths: [
+      {
+        name: "Quem Somos",
+        pathname: "/institucional/quem-somos",
+      },
+      {
+        name: "Histórico",
+        pathname: "/institucional/historico",
+      },
+    ],
   },
   {
     name: "Legislação",
@@ -56,7 +71,20 @@ const menus = [
 
 function MenuTop() {
   const pathname = usePathname();
-
+  console.log("pathname", pathname);
+  function isActive(menu: MenuItem): boolean {
+    console.log("first", menu);
+    if (menu.paths) {
+      console.log(
+        "menu.paths",
+        menu.paths.some((subMenu) => pathname.includes(subMenu.pathname))
+      );
+      // Verifica se o pathname da página está dentro de algum submenu
+      return menu.paths.some((subMenu) => pathname.includes(subMenu.pathname));
+    }
+    // Caso não haja submenu, verifica se o pathname corresponde ao item principal
+    return menu.pathname ? pathname === menu.pathname : false;
+  }
   return (
     <div>
       <div className="w-full bg-black">
@@ -93,23 +121,62 @@ function MenuTop() {
           </div>
         </div>
       </div>
-      <div className="container mt-[1.86rem] ">
+      <div className="container mt-[1.86rem]">
         <div className="max-w-[99.3125rem] mx-auto w-full flex justify-between px-[1.64rem]">
           {menus.map((menu, index) => (
-            <div
-              key={index}
-              className="pb-[1.86rem] hover:border-b-[0.125rem] hover:border-[#006A9E]"
-              style={{
-                borderBottom:
-                  pathname === menu.pathname ? "0.125rem solid #006A9E" : "",
-                fontWeight: pathname === menu.pathname ? "700" : "",
-              }}
-            >
-              <Link href={menu.pathname}>
-                <button className="inline-block py-[0.88rem] px-[1.25rem] text-[#002A3E] hover:font-bold text-[0.95769rem] leading-[0.95769rem]">
-                  {menu.name}
-                </button>
-              </Link>
+            <div key={index}>
+              {menu.pathname ? (
+                <Link href={menu.pathname}>
+                  <div
+                    className="pb-[1.86rem] hover:border-b-[0.125rem] hover:border-[#006A9E] group"
+                    style={{
+                      borderBottom: isActive(menu)
+                        ? "0.125rem solid #006A9E"
+                        : "",
+                      fontWeight: isActive(menu) ? "700" : "",
+                    }}
+                  >
+                    <button className="inline-block py-[0.88rem] px-[1.25rem] text-[#002A3E] group-hover:font-bold text-[0.95769rem] leading-[0.95769rem]">
+                      {menu.name}
+                    </button>
+                  </div>
+                </Link>
+              ) : (
+                <div
+                  className="pb-[1.86rem] hover:border-b-[0.125rem] hover:border-[#006A9E] group w-[9.9375rem]"
+                  style={{
+                    borderBottom: isActive(menu)
+                      ? "0.125rem solid #006A9E"
+                      : "",
+                    fontWeight: isActive(menu) ? "700" : "",
+                  }}
+                >
+                  <div className=" relative z-10">
+                    <p className="inline-block py-[0.88rem] px-[1.25rem] text-[#002A3E] text-center group-hover:font-bold text-[0.95769rem] leading-[0.95769rem] w-full ">
+                      {menu.name}
+                    </p>
+                    <div className="absolute top-[4.6rem] w-full hidden group-hover:block">
+                      <div className="bg-[#fff] w-[100%] rounded-[0.8125rem] py-[0.94rem] h-[5.625rem]">
+                        {menu.paths &&
+                          menu.paths.map((path, i) => (
+                            <Link
+                              key={i}
+                              href={path.pathname}
+                              className="block px-[1.25rem] py-[0.625rem] hover:bg-[#006A9E] hover:text-[#fff] text-[0.95769rem] leading-[0.95769rem] text-[#000000] "
+                              style={{
+                                fontWeight: pathname.includes(path.pathname)
+                                  ? "700"
+                                  : "400",
+                              }}
+                            >
+                              {path.name}
+                            </Link>
+                          ))}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
           ))}
         </div>
