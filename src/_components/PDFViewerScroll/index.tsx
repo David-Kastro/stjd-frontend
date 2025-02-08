f0f2f774cfc1a581577587e8466f5b042318243a
Mateus Pires <mateus.dev.ti@gmail.com>
2025-02-08 20:58:21 -0300

    fix: ajustes pdf

--- src/_components/PDFViewerScroll/index.tsx
+++ src/_components/PDFViewerScroll/index.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { Document, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/esm/Page/TextLayer.css'
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
+import { Button } from '../ui/button'
+import { Skeleton } from '../ui/skeleton'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
 
@@ -40,25 +42,47 @@ const PDFViewerScroll = ({ linkPdf }: PDFViewerProps) => {
         </div>
       </div>
 
-      {loading && <p>Carregando documento...</p>}
-
-      <div
-        className="scroll-custom-editais mx-auto mt-[1rem] flex min-h-[28rem] w-full max-w-[46rem] justify-center overflow-y-scroll border border-gray-300 bg-white p-2 shadow-lg"
-        onScroll={handleScroll}
-      >
-        <Document file={linkPdf} onLoadSuccess={onDocumentLoadSuccess}>
-          {Array.from(new Array(numPages), (_, index) => (
-            <div key={`page_${index + 1}`} className="mb-4">
-              <Page
-                pageNumber={index + 1}
-                scale={1.0}
-                renderTextLayer={false}
-                renderAnnotationLayer={false}
-              />
-            </div>
-          ))}
-        </Document>
-      </div>
+      {loading && (
+        <div className="flex h-full flex-col items-center justify-center">
+          <Skeleton className="h-[27rem] w-[46rem] bg-[#ccc]" />
+          <div className="mt-5 flex justify-center">
+            <Button
+              disabled
+              className="mx-auto h-[3.75rem] rounded-[4.625rem] px-[3.12rem] text-[1.25rem] font-bold"
+            >
+              Fazer Download
+            </Button>
+          </div>
+        </div>
+      )}
+      {!loading && (
+        <div>
+          <div
+            className="scroll-custom-editais mx-auto mt-[1rem] flex min-h-[27rem] w-full max-w-[46rem] justify-center overflow-y-scroll border border-gray-300 bg-white p-2 shadow-lg"
+            onScroll={handleScroll}
+          >
+            <Document file={linkPdf} onLoadSuccess={onDocumentLoadSuccess}>
+              {Array.from(new Array(numPages), (_, index) => (
+                <div key={`page_${index + 1}`} className="mb-4">
+                  <Page
+                    pageNumber={index + 1}
+                    scale={1.0}
+                    renderTextLayer={false}
+                    renderAnnotationLayer={false}
+                  />
+                </div>
+              ))}
+            </Document>
+          </div>
+          <div className="mt-5 flex justify-center">
+            <a href={linkPdf} className="mx-auto w-fit" target="_blank">
+              <Button className="mx-auto h-[3.75rem] rounded-[4.625rem] px-[3.12rem] text-[1.25rem] font-bold">
+                Fazer Download
+              </Button>
+            </a>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
