0b3b289f7c66a30b10a3f6ee8ff00d5c39ba15b8
Mateus Pires <mateus.dev.ti@gmail.com>
2025-02-08 22:42:10 -0300

    finalizando pagina

--- src/app/transparencia-pretacao-contas/page.tsx
+++ src/app/transparencia-pretacao-contas/page.tsx
@@ -1,24 +1,119 @@
+import CardTranparencia from '@/_components/CardTranparencia'
+import ScaleAttorneys from '@/_components/ScaleAttorneys'
 import { Scale } from 'lucide-react'
+import BgScalle from '/public/images/bg-card-scale.svg'
 import React from 'react'
+import Image from 'next/image'
+import LogoBlack from '/public/images/logo-stjd-black.svg'
 
 function Transparencia() {
+  const acervos = [
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+    {
+      valor: '22.449',
+      textContent: 'processos em tramitação.',
+    },
+  ]
+
   return (
-    <div className="container mt-[5.57rem] border-l">
-      <div className="mx-auto w-full max-w-[100.82681rem]">
-        <div className="bg-painel-transparencia h-[56.625rem] w-full max-w-[21rem] rounded-[1.375rem]">
-          <div className="flex items-center gap-[0.56rem] pl-[2.19rem] pt-[2rem]">
-            <Scale />
-            <p className="text-[1.25rem] font-bold leading-[1.23775rem]">
-              Painéis Estáticos
-            </p>
-          </div>
-          <hr className="mt-[2.12rem] h-[0.125rem] border-[#BD995D] bg-[#BD995D]" />
-          <div className="relative mt-[3.43rem]">
-            <div className="absolute -left-[2px] h-[1.7925rem] w-[5px] rounded-full bg-[#BD995D]"></div>
-            <div className="m pl-[1.72rem]">
-              <p className="text-[0.95769rem] text-[#393939]">
-                Informações atualizadas diariamente.
+    <div className="container mt-[5.57rem]">
+      <div className="lg:border-l-[2px]">
+        <div className="mx-auto flex w-full max-w-[100.82681rem] items-center gap-[2.94rem]">
+          <div className="bg-painel-transparencia h-[56.625rem] w-full max-w-[21rem] rounded-[1.375rem]">
+            <div className="flex items-center gap-[0.56rem] pl-[2.19rem] pt-[2rem]">
+              <Scale />
+              <p className="text-[1.25rem] font-bold leading-[1.23775rem]">
+                Painéis Estáticos
               </p>
+            </div>
+            <hr className="mt-[2.12rem] h-[0.125rem] border-[#BD995D] bg-[#BD995D]" />
+            <div className="relative mt-[3.43rem]">
+              <div className="absolute -left-[2px] h-[1.7925rem] w-[5px] rounded-full bg-[#BD995D]"></div>
+              <div className="m pl-[1.72rem]">
+                <p className="text-[0.95769rem] text-[#393939]">
+                  Informações atualizadas diariamente.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="grid w-full grid-cols-4 gap-x-[1.1rem] gap-y-[1.58rem]">
+            {acervos.map((acervo, index) => (
+              <CardTranparencia
+                key={index}
+                valor={acervo.valor}
+                textContent={acervo.textContent}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="mx-auto w-full max-w-[100.82681rem]">
+          <hr className="mb-[4.31rem] mt-[3.15rem] h-[0.125rem] bg-secondary"></hr>
+          <ScaleAttorneys
+            title="Escala de Procuradores 2024"
+            subtitle="COMPETIÇÕES PROMOVIDAS PELA CBF"
+            height="8.125rem"
+            image={BgScalle}
+            textbtn="Saiba Mais"
+            href=""
+          />
+          <div className="mt-[8rem] bg-[#000] py-[3.87rem] lg:container lg:bg-transparent lg:py-0">
+            <div className="lg:pb-[7.94rem]">
+              <Image
+                src={LogoBlack}
+                alt="LogoBlack"
+                className="mx-auto w-[8.9375rem] lg:w-auto"
+              />
             </div>
           </div>
         </div>
